e0304f422c5a6c7ad5f4f87ddcde6685ae795bdf
Piyush Chaudhari <the.piyush.chaudhari@gmail.com>
2026-06-30 14:16:52 +0530

    Add files via upload

---
+++ Daily Practice/AdditionTypeCastX.py
@@ -0,0 +1,9 @@
+print("Enter 1st number : ")
+No1 = int(input())
+
+print("Enter 2nd number : ")
+No2 = int(input())
+
+Ans = No1 + No2
+
+print("Addition is : ",Ans)
---
+++ Daily Practice/AdditionVariable.py
@@ -0,0 +1,12 @@
+No1 = 0
+No2 = 0
+
+print("Enter 1st number : ")
+No1 = input()
+
+print("Enter 2nd number : ")
+No2 = input()
+
+Ans = No1 + No2
+
+print("Addition is : ",Ans)
