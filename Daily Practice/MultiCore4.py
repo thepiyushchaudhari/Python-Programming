7498105d924bc7cd8baadb35f5cafaa1f0d6ee44
Piyush Chaudhari <the.piyush.chaudhari@gmail.com>
2026-07-11 17:01:10 +0530

    Add files via upload

---
+++ Daily Practice/MultiCore4.py
@@ -0,0 +1,33 @@
+import time, multiprocessing, os
+
+def SumCube (No):
+    print("Process is Running with PID : ", os.getpid())
+    Sum = 0
+    
+    for i in range (1, No+1):
+        Sum = Sum + (i ** 3)
+    return Sum
+
+def main ():
+    
+    Data = [10000000, 20000000, 30000000, 40000000, 50000000]
+    Result = []
+
+    start_time = time.perf_counter()
+    
+    pobj = multiprocessing.Pool()    
+
+    Result = pobj.map(SumCube, Data)
+    
+    pobj.close()
+    pobj.join()
+    
+    end_time = time.perf_counter()
+    
+    print("Result is : ")
+    print(Result)
+
+    
+    print(f"Time required : {end_time - start_time} seconds")
+if __name__ == "__main__" :
+    main()
