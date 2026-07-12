2c66e7e97bc7cac31b1c96001243b338fa1237e8
Piyush Chaudhari <the.piyush.chaudhari@gmail.com>
2026-07-12 23:13:29 +0530

    Add files via upload

---
+++ Daily Practice/ExceptionDemo6.py
@@ -0,0 +1,29 @@
+def main ():
+    Ans = 0
+    try:
+        print ("Enter first number : ")
+        No1 = int(input())
+        
+        print ("Enter second number : ")
+        No2 = int(input())
+    
+        Ans = No1 / No2
+        
+        print("Division is successfull")
+
+    except ZeroDivisionError as zobj :              #Specific Exception -> works for this error only 
+        print("Exception Occured due to second operand is ZERO : ", zobj)
+    
+    except ValueError as vobj :                     #Specific Exception -> works for this error only 
+        print("Exception occured due to invalid data type : ", vobj)
+    
+    except Exception as eobj :                      #Generic Exception -> works for any error while run time
+        print("Exception occured : ", eobj)
+    
+    finally :
+        print("Inside Finally Block")  
+
+    print("Result is : ", Ans)  
+
+if __name__ == "__main__":
+    main()
---
+++ Daily Practice/Functional.py
@@ -0,0 +1,14 @@
+Addition = lambda No1, No2: No1 + No2
+Substraction = lambda No1, No2: No1 - No2
+
+print("Enter First Number : ")
+Num1 = int(input())
+
+print("Enter First Number : ")
+Num2 = int(input())
+
+Ret = Addition(Num1, Num2)
+print("Addition is : ", Ret)
+
+Ret = Substraction(Num1, Num2)
+print("Substraction is : ", Ret)
